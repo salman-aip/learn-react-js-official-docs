532e511e5a2e5098d28caf2cc7ef3bb8111d6519
Salman Farshee <salman@aveniripltd.com>
2023-09-18 17:52:14 +0600

    reset state with key

--- src/app/useState/page.tsx
+++ src/app/useState/page.tsx
@@ -1,9 +1,12 @@
+import ResetStateWithKey from "./reset-state-with-key";
 import UseStateObject from "./state-object";
 
 export default function UseState() {
   return (
     <>
       <UseStateObject />
+
+      <ResetStateWithKey />
     </>
   );
 }
