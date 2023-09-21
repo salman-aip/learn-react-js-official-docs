ec7d4d0d5abf39fd9876d6fca1916543d0147fa4
Salman Farshee <salman@aveniripltd.com>
2023-09-21 14:37:31 +0600

    initialize context api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ContextAPI from "@/hooks/context-api";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -13,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
-        {children}
+        <ContextAPI>{children}</ContextAPI>
       </body>
     </html>
   );
